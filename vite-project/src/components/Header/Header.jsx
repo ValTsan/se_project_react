b1566bdd3fcf90ace0fa024bc699c3b478edfd8c
ValerieTsan <valeriejean.tsan@gmail.com>
2024-12-11 00:02:44 -0500

    Add structure and components

---
+++ vite-project/src/components/Header/Header.jsx
@@ -0,0 +1,37 @@
+//import React from "react";
+import "./Header.css";
+import headerLogo from "../../assets/logo.svg";
+import headerAvatar from "../../assets/Avatar.png";
+
+function Header({ handleAddClick }) {
+  const currentDate = new Date().toLocaleString("default", {
+    month: "long",
+    day: "numeric",
+  });
+  //const location = info ? info.location : null;
+  const userName = "Terence Tegegne";
+
+  return (
+    <header className="header">
+      <img src={headerLogo} alt="Header Logo" className="header__logo" />
+      <p className="header__date">{currentDate}</p>
+      <button
+        onClick={handleAddClick}
+        type="button"
+        className="header__add-clothes-btn"
+      >
+        +Add clothes{" "}
+      </button>
+      <div className="header__user-container">
+        <p className="header__username">Terrence Tegegne</p>
+        <img
+          src={headerAvatar}
+          alt="Terrence Tegegne"
+          className="header__avatar"
+        />
+      </div>
+    </header>
+  );
+}
+
+export default Header;
