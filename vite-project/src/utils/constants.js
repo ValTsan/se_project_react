0da9ef0c69a684b01adcb8d2a3f7a3bab07fb52a
ValerieTsan <valeriejean.tsan@gmail.com>
2024-12-12 13:37:21 -0500

    Fix fonts

--- vite-project/src/utils/constants.js
+++ vite-project/src/utils/constants.js
@@ -2,7 +2,7 @@ export const weatherOptions = [
   {
     day: true,
     condition: "clear",
-    url: new URL("../assets/day/sunny.svg", import.meta.url).href,
+    url: new URL("../assets/day/clear.svg", import.meta.url).href,
   },
   {
     day: true,
