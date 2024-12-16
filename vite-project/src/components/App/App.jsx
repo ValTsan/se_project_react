debbeb3edf8311a1a1f10b5c6e910e1132ddd920
ValerieTsan <valeriejean.tsan@gmail.com>
2024-12-16 11:55:22 -0500

    Added footer.jsx

--- vite-project/src/components/App/App.jsx
+++ vite-project/src/components/App/App.jsx
@@ -7,6 +7,7 @@ import Main from "../Main/Main";
 import ModalWithForm from "../ModalWithForm/ModalWithForm";
 import ItemModal from "../ItemModal/ItemModal";
 import { getWeather, filterWeatherData } from "../../utils/weatherApi";
+import Footer from "../Footer/Footer";
 
 function App() {
   const [weatherData, setWeatherData] = useState({
@@ -99,6 +100,7 @@ function App() {
         card={selectedCard}
         handleCloseClick={closeActiveModal}
       />
+      <Footer />
     </div>
   );
 }
---
+++ vite-project/src/components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <footer className="footer">
+      <h2 className="footer__header">Developed by Val Tsan</h2>
+      <p className="footer__date"></p>
+    </footer>
+  );
+}
+
+export default Footer;
