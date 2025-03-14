d0e5aae76d98e4d344cd98624020cde538df1f7c
ValerieTsan <valeriejean.tsan@gmail.com>
2025-03-14 13:24:48 -0400

    Fix dependencies

--- src/components/ProtectedRoute/ProtectedRoute.js
+++ src/components/ProtectedRoute/ProtectedRoute.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ element, isLoggedIn }) => {
-  return isLoggedIn ? element : <Navigate to="/" replace />;
+  //return isLoggedIn ? element : <Navigate to="/" replace />;
 };
 
 export default ProtectedRoute;
